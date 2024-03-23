b58e115e3a2ed60e85556c117bc6da4fb5f08ae3
Ganesh <ganeshsbsa08@gmail.com>
2024-03-23 19:33:59 +0530

    add some margin to logo

--- qtify/src/Component/Logo/Logo.js
+++ qtify/src/Component/Logo/Logo.js
@@ -1,9 +1,13 @@
 import React from "react";
 import LogoImg from '../../asset/logo.png';
+import styles from "./Logo.module.css"
 
 const Logo=()=>{
     return(
-        <img src={LogoImg} alt="logo" width={67} height={34}/>
+        <div className={styles.logo}>
+                <img src={LogoImg} alt="logo" width={67} height={34}/>
+        </div>
+     
     )
 }
 export default Logo
